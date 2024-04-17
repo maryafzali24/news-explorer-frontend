68f3d6466a34d7a5e0eb9b8789f3508557f583bd
maryafzali24 <123387020+maryafzali24@users.noreply.github.com>
2024-04-16 22:54:03 -0400

    fix: screensize on different layout

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,7 +3,7 @@ import notFoundImage from "../../images/not-found.svg";
 const PageNotFound = () => {
   return (
     <section className="not-found">
-      <img src={notFoundImage} className="not-found__image" alt="Sad face" />
+      <img className="not-found__image" src={notFoundImage} alt="not-found" />
       <h3 className="not-found__title">Nothing Found</h3>
       <p className="not-found__text">
         Sorry, but nothing matched your search terms.
